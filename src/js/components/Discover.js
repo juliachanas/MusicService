fdf0f3665d299cf14bc6875dd189f7cc4846af92
Julia Chanas <juulia.chanas@gmail.com>
2023-11-05 17:23:53 +0100

    Add randomSong method to Discover class

--- src/js/components/Discover.js
+++ src/js/components/Discover.js
@@ -21,6 +21,22 @@ class Discover {
     const generatedHTML = templates.discover();
 
     thisDiscover.container.innerHTML = generatedHTML;
+
+    thisDiscover.randomSong();
+  }
+
+  randomSong() {
+    const thisDiscover = this;
+
+    //losujemy index piosenki - math.random losuje liczby od 0-1, mnożę przez ilośc piosenek w tablicy(lenght) - zeby otrzymać zakres <0,4) i zaokrąglam w dół funckją math.floor
+    thisDiscover.randomSongIndex = Math.floor(
+      Math.random() * thisDiscover.songs.length
+    );
+    console.log('wylosowany index piosenki: ', thisDiscover.randomSongIndex);
+
+    //sprawdzam jaka piosenka kryje się pod wylosowanym indeksem
+    thisDiscover.randomSong = thisDiscover.songs[thisDiscover.randomSongIndex];
+    console.log('wylosowana losowa piosenka:', thisDiscover.randomSong);
   }
 }
 
